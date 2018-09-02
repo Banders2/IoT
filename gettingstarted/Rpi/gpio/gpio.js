010630c8a99b889023fe2e2669a835378cb4432c
NicklasNielsen <you@example.com>
2018-09-02 17:10:16 +0200

    updates

--- gettingstarted/Rpi/gpio/gpio.js
+++ gettingstarted/Rpi/gpio/gpio.js
@@ -14,7 +14,9 @@ module.exports.ledOff = () => {
 
 module.exports.ledToggle = () => {
     var led = new Gpio(4, 'out');
-    led.writeSync((led.readSync() + 1) % 2);
+    var ledstate = (led.readSync() + 1) % 2;
+    console.log('Led is now: ' + ledstate);
+    led.writeSync(ledstate);
 }
 
 module.exports.humidityRead = () => {
@@ -29,13 +31,17 @@ module.exports.temperatureRead = () => {
     return readout.temperature.toFixed(2) + 'C';
 }
 
+var pirval = 'No value';
+
 module.exports.pirRead = () => {
     var pir = new Gpio(17, 'in', 'both');
-    var pirval = 'No value';
+    
     pir.watch((err, val) => {
         if(err) throw err;
+        
         console.log('Pir: ' + val);
         pirval = val.toString() + ' current value';
     })
+
     return pirval;
 }
