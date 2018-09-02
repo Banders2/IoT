da4eb2cb468da95449ec897340e8afedb312cc92
NicklasNielsen <you@example.com>
2018-09-02 14:51:34 +0200

    More test with onoff

--- gettingstarted/Rpi/gpio/gpio.js
+++ gettingstarted/Rpi/gpio/gpio.js
@@ -9,3 +9,15 @@ module.exports.ledOff = () => {
     var led = new Gpio(4, 'out');
     led.writeSync(0);
 }
+
+module.exports.humidityRead = () => {
+
+}
+
+module.exports.temperatureRead = () => {
+    
+}
+
+module.exports.pirRead = () => {
+    
+}
